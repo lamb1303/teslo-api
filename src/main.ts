03426197709d0aac448e3ddacc6ed8114ebcbbe3
lamb1303 <lamb1303@outlook.com>
2023-03-10 17:57:32 -0600

    feat: :sparkles: Upload files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +13,7 @@ async function bootstrap() {
   );
 
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
